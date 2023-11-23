7d51823fffe4e30bdbc982d51d42c5cc71956845
sherzod555 <wiut00007584@gmail.com>
2023-11-23 15:18:18 +0500

    yeeeeptaa

--- src/components/Main/Sidebar.jsx
+++ src/components/Main/Sidebar.jsx
@@ -93,10 +93,10 @@ export const Sidebar = () => {
         <div className={`w-full h-full ${isDarkMode ? 'filter invert bg-white/70' : 'bg-black/70'} absolute z-20 top-0 ${isOpen? 'block' : 'hidden'} `}>
                <div className='w-[80%] md:w-[50%]  lg:w-[30%] mx-auto mt-10 md:mt-20 bg-[#e3e3e3] rounded-2xl p-4  md:p-5 flex flex-col items-center gap-y-4 md:gap-y-5'>
                 <p className='py-2 px-3 bg-gray-400 rounded-xl'>
-                  username: Username
+                  Your e-mail: Username
                 </p>
                 <p className='py-2 px-3 bg-gray-400 rounded-xl'>
-                  password: user password
+                  Your password: user password
                 </p>
 
                 <button onClick={()=>setIsOpen(false)} className='py-2 w-full bg-black rounded-xl text-white z-40'>
